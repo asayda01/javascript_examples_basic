5038f4efe72ce44c13ec2d8d7fc26a6b41b2bdb1
Cem Saydam <ahmetcemsaydam@gmail.com>
2022-12-20 14:38:14 +0000

    added initial version of some functions
    added initial version of the functions below as commentary:
    
    - function getTotalSubjects(people)
    
    - function checkIngredients(menu, ingredient)
    
    - function duplicateNumbers(arr1, arr2)

--- basic_exercise_03.js
+++ basic_exercise_03.js
@@ -12,6 +12,32 @@ export function camelCaseWords(words) {
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
+  
+    /*
+  
+  let tot1 = 0;
+  for (let i of people) {
+    tot1 += i.subjects.length
+    };
+  return tot1;
+  
+  */
+  
+  /*
+  let tot2 = 0;
+  people.forEach((x) => tot2 += x.subjects.length);
+  return tot2;
+  
+  */
+  
+  /*
+  
+  let tot3 = 0;
+  people.map(x => {return tot3 += x.subjects.length}) ;
+  return tot3;
+  
+  */
+
   let count_1 = people.map(people => { return people.subjects.length; });
   return count_1.reduce((a, b) => a + b, 0);
 }
@@ -19,11 +45,43 @@ export function getTotalSubjects(people) {
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
+
+  /*
+
+  for (let i of menu) {
+  if (i.ingredients.includes(ingredient) === true) {
+    return true}
+  };
+  return false;
+
+  */
+
   return menu.some(x => x.ingredients.includes(ingredient) === true);
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
+
+  /*
+  
+  let set1 = new Set();
+  for (let x of arr1) {
+    for (let y of arr2) { 
+      if (x === y) {
+        set1.add(x)}
+    }
+  }
+  return Array.from(set1);
+  
+  */
+  
+  /*
+  
+  let new_arr = arr1.filter(x => {return arr2.indexOf(x) > -1 });
+  return [... new Set( new_arr.sort()) ];
+  
+  */
+
   return [...new Set(arr1)].filter(Set.prototype.has, new Set(arr2)).sort();
 }
