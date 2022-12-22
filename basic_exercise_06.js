14185e62e465f59a168a05621332e3ccb80440e1
Cem Saydam <ahmetcemsaydam@gmail.com>
2022-12-22 13:24:29 +0000

    added initial version of some functions
     added initial version of some functions
    
    added initial version of the functions below as commentary:
    
    - function sumMultiples (arr)
    
    - function isValidDNA (str)
    
    - function getComplementaryDNA (str)
    
    - function createMatrix (n, fill)

--- basic_exercise_06.js
+++ basic_exercise_06.js
@@ -6,8 +6,47 @@
  */
 export function sumMultiples (arr) {
   if (arr === undefined) throw new Error("arr is required");
+
+  /*
+  
+  let sum = 0;
+  for (let x of arr) {
+    if ((x % 3 === 0) || (x % 5 === 0)) {
+      sum += x}
+    }
+  return sum;
+
+ */
+
+  /*
+
+  let sum = 0;
+  let arr2 = arr.filter(x => {return ((x % 3 === 0) || (x % 5 === 0)) })
+  arr2.forEach( (x) => sum += x);
+  return sum;
+
+  */
+
+  /*
+
+  let sum = 0;
+  (arr.filter(x => {return ((x % 3 === 0) || (x % 5 === 0)) })).forEach( (x) => sum += x);
+  return sum;
+
+  */
+
   let arr_factorized = arr.filter(x => {return (x % 3 === 0) || (x % 5 === 0) } );
   return arr_factorized.reduce((a, b) => a + b, 0);
+
+  /*
+
+  one-liner solution :
+  
+  return (arr.filter(x => 
+  {return ((x % 3 === 0) || (x % 5 === 0)) })).
+  reduce ( (a,b) => a+b, 0 );  
+
+  */
 };
 
 /**
@@ -19,6 +58,22 @@ export function sumMultiples (arr) {
  */
 export function isValidDNA (str) {
   if (str === undefined) throw new Error("str is required");
+
+  /*
+
+  let arr1 = str.split("");
+  let arr2 = arr1.map(x => 
+      {return (   (x.toUpperCase()==="C") ||
+        (x.toUpperCase()==="G") ||
+        (x.toUpperCase()==="T") ||
+        (x.toUpperCase()==="A")              )
+                                                } );
+  let arr3 = arr2.some(x => 
+    {return String(x).includes(false) === true});
+  return !arr3;
+
+  */
+
   let arr_1 = str.split("").map(x => {return ((x.toUpperCase()==="C") ||
   (x.toUpperCase()==="G") || (x.toUpperCase()==="T") || (x.toUpperCase()==="A")) });
   return !arr_1.some(x => {return (x.toString()).includes(false) === true});
@@ -34,6 +89,32 @@ export function isValidDNA (str) {
  */
 export function getComplementaryDNA (str) {
   if (str === undefined) throw new Error("str is required");
+
+  /*
+
+  let arr = str.split("");
+  
+  for (let x =0; x < arr.length; x++) {
+    
+    if ( arr[x].toUpperCase() === 'A') 
+    { arr[x] = arr[x].replace(arr[x], 'T') }
+    
+    else if ( arr[x].toUpperCase() === 'C') 
+    { arr[x] = arr[x].replace(arr[x], 'G' ) }
+    
+    else if ( arr[x].toUpperCase() === 'G') 
+    { arr[x] = arr[x].replace(arr[x], 'C' )}
+    
+    else if ( arr[x].toUpperCase() === 'T') 
+    { arr[x] = arr[x].replace(arr[x], 'A' ) }
+    
+    };
+
+  let str1 = arr.join("");
+  return str1;
+
+  */
+
   return (str.split("").map(x => { 
     if (x.toUpperCase()==="G") return "C";
     else if (x.toUpperCase()==="C") return "G";
@@ -54,8 +135,9 @@ export function getComplementaryDNA (str) {
 export function isItPrime (n) {
   if (n === undefined) throw new Error("n is required");
   if (n <= 1) return false;    // negative numbers, 0 and 1 are not prime numbers.
-  for(let i = 2, s = Math.sqrt(n); i <= s; i++)
-      if(n % i === 0) return false; 
+  for(let i = 2, s = Math.sqrt(n); i <= s; i++) {
+      if(n % i === 0) return false;
+  }; 
     return n > 1;
 };
 
@@ -76,6 +158,41 @@ export function isItPrime (n) {
 export function createMatrix (n, fill) {
   if (n === undefined) throw new Error("n is required");
   if (fill === undefined) throw new Error("fill is required");
+
+  /*
+
+  let arr1 = [];
+  let arr2 = [];
+  for (let i = 0; i< n ; i++) {
+    arr1.push(fill)}
+  for (let j = 0; j< n ; j++) {
+    arr2.push(arr1)}
+  return arr2;
+
+  */
+
+  /*
+
+  let arr1 = [];
+  let arr2 = [];
+  for (let i = 0; i< n ; i++) {
+    arr1.push(fill);
+    arr2.push(arr1);
+    }
+  return arr2;
+
+  */
+
+  /*
+
+  let arr1 = [];
+  for (let i = 0; i< n ; i++) {
+    arr1.push( new Array(n).fill(fill)  );
+    }
+  return arr1;
+
+  */
+
   let arr = [];
   for (let i = 0; i < n; i++) { arr.push(new Array(n).fill(fill))};
   return arr;
