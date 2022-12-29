c3dd743caf20f0fb3f13f630eb28ce50008c3625
asayda01 <ahmetcemsaydam@gmail.com>
2022-12-29 11:40:55 +0000

    created basic_exercise_08.js
    created basic_exercise_08 file in javascript for this repo.

---
+++ basic_exercise_08.js
@@ -0,0 +1,28 @@
+// Spread Syntax Bonus activity
+
+export function printColours(colours) {
+    if (!colours) throw new Error("colour is required");
+    return [... colours];
+  }
+  
+  export function shoppingList(list, newItem) {
+    if (!list) throw new Error("list is required");
+    if (!newItem) throw new Error("newItem is required");
+    return [... list, ...newItem];
+  }
+  
+  export function highestNumber(numbers) {
+    if (!numbers) throw new Error("numbers are required");
+    return Math.max(... numbers);
+  }
+  
+  export function splitThatString(string) {
+    if (!string) throw new Error("string is required");
+    return [... string];
+  }
+  
+  export function addressLookUp(user) {
+    if (!user) throw new Error("user is required");
+    return user.address?.postcode;
+  }
+  
